feb31c825ce6ba56f591e48eb48b20786d7b3c8a
Raul Rodriguez Ramos <raul-rodriguezz@hotmail.com>
2025-02-18 00:20:21 -0600

    Proyecto

--- src/main/java/com/mycompany/topicosavanzados/TopicosAvanzados.java
+++ src/main/java/com/mycompany/topicosavanzados/TopicosAvanzados.java
@@ -8,9 +8,69 @@ package com.mycompany.topicosavanzados;
  *
  * @author raul
  */
-public class TopicosAvanzados {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class TopicosAvanzados implements ActionListener {
+    private JLabel etiqueta1;
+    private JTextField campoDeTexto;
+    private JButton boton;
+    private JLabel etiqueta2;
+
+    public TopicosAvanzados() {
+        // *********************************************
+        // CREACIÓN DEL INTERFAZ GRÁFICO
+        // *********************************************
+        // Crear la ventana de la aplicacion
+        JFrame ventana = new JFrame(
+                "Conversor de Euros a pesetas");
+        ventana.setSize(300, 200);
+        ventana.setDefaultCloseOperation(
+                JFrame.EXIT_ON_CLOSE);
+        // Crear los componentes
+        etiqueta1 = new JLabel("Importe en Euros");
+        campoDeTexto = new JTextField(20);
+        boton = new JButton("Convertir");
+        etiqueta2 = new JLabel("Pulse para obtener el "
+                + "importe en pesetas");
+        // Crear un contenedor
+        JPanel panelDeContenido = new JPanel();
+        // Configurar el contenedor para mostrar los
+        // componentes cuando se muestre.
+        panelDeContenido.add(etiqueta1);
+        panelDeContenido.add(campoDeTexto);
+        panelDeContenido.add(boton);
+        panelDeContenido.add(etiqueta2);
+        // Configurar la ventana para mostrar el panel
+        // cuando se muestre
+        ventana.setContentPane(panelDeContenido);
+        // *********************************************
+        // ASOCIACIÓN DEL CÓDIGO DE MANEJO DE EVENTOS
+        // *********************************************
+        boton.addActionListener(this);
+        // *********************************************
+        // HACER VISIBLE LA VENTANA
+        // *********************************************
+        ventana.setVisible(true);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new TopicosAvanzados();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // **********************************************
+        // CÓDIGO DE MANEJO DE EVENTOS
+        // **********************************************
+        try {
+            double euros = Double.parseDouble(
+                    campoDeTexto.getText());
+            double pesetas = euros * 166.386;
+            etiqueta2.setText("Equivale a " + pesetas
+                    + " pesetas");
+        } catch (NumberFormatException e2) {
+            etiqueta2.setText(
+                    "En el campo de texto no hay un número");
+        }
     }
 }
